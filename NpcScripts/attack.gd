4b43d4e710a084aecc9e1208d061cab6b2ed9935
grymestone <grymestone@ufl.edu>
2023-06-11 15:14:49 -0400

    Best not let things do negative damage...

--- NpcScripts/attack.gd
+++ NpcScripts/attack.gd
@@ -38,17 +38,38 @@ static func getAttack(char_class):
 
 static func attackTarget(attacking_node, target_node):
 	var physical_damage = attacking_node.attack["Physical"] - target_node.stats["Physical Resistance"]
+	if physical_damage < 0:
+		physical_damage = 0
 	var magic_damage = attacking_node.attack["Magic"] - target_node.stats["Magic Resistance"]
+	if magic_damage < 0:
+		magic_damage = 0
 	var fire_damage = attacking_node.attack["Fire"] - target_node.stats["Fire Resistance"]
+	if fire_damage < 0:
+		fire_damage = 0
 	var air_damage = attacking_node.attack["Air"] - target_node.stats["Air Resistance"]
+	if air_damage < 0:
+		air_damage = 0
 	var earth_damage = attacking_node.attack["Earth"] - target_node.stats["Earth Resistance"]
+	if earth_damage < 0:
+		earth_damage = 0
 	var water_damage = attacking_node.attack["Water"] - target_node.stats["Water Resistance"]
+	if water_damage < 0:
+		water_damage = 0
 	var light_damage = attacking_node.attack["Light"] - target_node.stats["Light Resistance"]
+	if light_damage < 0:
+		light_damage = 0
 	var shadow_damage = attacking_node.attack["Shadow"] - target_node.stats["Shadow Resistance"]
+	if shadow_damage < 0:
+		shadow_damage = 0
 	var nature_damage = attacking_node.attack["Nature"] - target_node.stats["Nature Resistance"]
+	if nature_damage < 0:
+		nature_damage = 0
 	var electric_damage = attacking_node.attack["Electric"] - target_node.stats["Electric Resistance"]
+	if electric_damage < 0:
+		electric_damage = 0
 	var damage_dealt = physical_damage + magic_damage + fire_damage + air_damage + earth_damage + water_damage + light_damage + shadow_damage + nature_damage + electric_damage
-	
+	if damage_dealt < 0:
+		damage_dealt = 0
 	print(attacking_node.char_class, " is attacking ", target_node.char_class, "! They deal ", damage_dealt)
 	
 	target_node.current_health = target_node.current_health - damage_dealt
