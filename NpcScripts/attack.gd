6b7a4660ff2598b757121652309cd329709ae3ed
RussellRWG <russellrwg@gmail.com>
2023-06-11 23:40:53 -0400

    Adjusted to percentage damage reduction

--- NpcScripts/attack.gd
+++ NpcScripts/attack.gd
@@ -6,6 +6,7 @@ class_name Attacks
 static func getAttack(char_class):
 	var generic_attack = {
 	"Range": 10,
+	"Speed": 3,
 	"Physical" : 5,
 	"Magic": 0,
 	"Fire": 0,
@@ -20,6 +21,7 @@ static func getAttack(char_class):
 
 	var barbarian_attack = {
 		"Range": 25,
+		"Speed": 3,
 		"Physical" : 15,
 		"Magic": 0,
 		"Fire": 0,
@@ -37,34 +39,34 @@ static func getAttack(char_class):
 	return attack_lib[char_class]
 
 static func attackTarget(attacking_node, target_node):
-	var physical_damage = attacking_node.attack["Physical"] - target_node.stats["Physical Resistance"]
+	var physical_damage = attacking_node.attack["Physical"] * (1 - target_node.stats["Physical Resistance"])
 	if physical_damage < 0:
 		physical_damage = 0
-	var magic_damage = attacking_node.attack["Magic"] - target_node.stats["Magic Resistance"]
+	var magic_damage = attacking_node.attack["Magic"] * (1 - target_node.stats["Magic Resistance"])
 	if magic_damage < 0:
 		magic_damage = 0
-	var fire_damage = attacking_node.attack["Fire"] - target_node.stats["Fire Resistance"]
+	var fire_damage = attacking_node.attack["Fire"] * (1 - target_node.stats["Fire Resistance"])
 	if fire_damage < 0:
 		fire_damage = 0
-	var air_damage = attacking_node.attack["Air"] - target_node.stats["Air Resistance"]
+	var air_damage = attacking_node.attack["Air"] * (1 - target_node.stats["Air Resistance"])
 	if air_damage < 0:
 		air_damage = 0
-	var earth_damage = attacking_node.attack["Earth"] - target_node.stats["Earth Resistance"]
+	var earth_damage = attacking_node.attack["Earth"] * (1 - target_node.stats["Earth Resistance"])
 	if earth_damage < 0:
 		earth_damage = 0
-	var water_damage = attacking_node.attack["Water"] - target_node.stats["Water Resistance"]
+	var water_damage = attacking_node.attack["Water"] * (1 - target_node.stats["Water Resistance"])
 	if water_damage < 0:
 		water_damage = 0
-	var light_damage = attacking_node.attack["Light"] - target_node.stats["Light Resistance"]
+	var light_damage = attacking_node.attack["Light"] * (1 - target_node.stats["Light Resistance"])
 	if light_damage < 0:
 		light_damage = 0
-	var shadow_damage = attacking_node.attack["Shadow"] - target_node.stats["Shadow Resistance"]
+	var shadow_damage = attacking_node.attack["Shadow"] * (1 - target_node.stats["Shadow Resistance"])
 	if shadow_damage < 0:
 		shadow_damage = 0
-	var nature_damage = attacking_node.attack["Nature"] - target_node.stats["Nature Resistance"]
+	var nature_damage = attacking_node.attack["Nature"] * (1 - target_node.stats["Nature Resistance"])
 	if nature_damage < 0:
 		nature_damage = 0
-	var electric_damage = attacking_node.attack["Electric"] - target_node.stats["Electric Resistance"]
+	var electric_damage = attacking_node.attack["Electric"] * (1 - target_node.stats["Electric Resistance"])
 	if electric_damage < 0:
 		electric_damage = 0
 	var damage_dealt = physical_damage + magic_damage + fire_damage + air_damage + earth_damage + water_damage + light_damage + shadow_damage + nature_damage + electric_damage
