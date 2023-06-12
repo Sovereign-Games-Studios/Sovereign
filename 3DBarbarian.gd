8be3ed46ba05da4bdc9119139d7488de4f1ba028
RussellRWG <russellrwg@gmail.com>
2023-06-12 18:05:58 -0400

    Adding 3D stuff

---
+++ 3DBarbarian.gd
@@ -0,0 +1,34 @@
+extends CharacterBody3D
+
+var current_path: PackedVector3Array
+var nav_map: RID
+var speed = 5
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	await get_tree().create_timer(0.1).timeout
+	nav_map = get_world_3d().get_navigation_map()
+	set_destination(Vector3(-71, 0, 0))
+	
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if $NavigationAgent3D.is_target_reachable():
+		var target = $NavigationAgent3D.get_next_path_position()
+		velocity = global_transform.origin.direction_to(target).normalized() * speed
+		$NavigationAgent3D.set_velocity(velocity)
+	else:
+		$NavigationAgent3D.set_velocity(Vector3.ZERO)
+	move_and_slide()
+	pass
+
+	
+func set_destination(new_destination:Vector3):
+	var destination = new_destination
+	$NavigationAgent3D.set_target_position(destination)
+
+func _on_navigation_agent_3d_velocity_computed(safe_velocity):
+	set_velocity(safe_velocity)
+	pass # Replace with function body.
---
+++ Camera3D.gd
@@ -0,0 +1,26 @@
+extends Camera3D
+
+var align_time = .2
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var desired_pos = Vector3.ZERO
+	if Input.is_action_pressed("A"):
+		desired_pos.x += -25 * delta
+	if Input.is_action_pressed("D"):
+		desired_pos.x += 25 * delta
+	if Input.is_action_pressed("W"):
+		desired_pos.z += -25 * delta
+	if Input.is_action_pressed("S"):
+		desired_pos.z += 25 * delta
+	self.transform.origin.x += desired_pos.x
+	self.transform.origin.z += desired_pos.z
+	
+	
+	#var tween = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUINT)
+	#tween.tween_property(self, "position", desired_pos, align_time).from_current()
+	pass
