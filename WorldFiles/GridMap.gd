2b7b5cb7d2f82b11265e235f7e6d5da293b67a5d
RustyBamboo <d@volya.xyz>
2023-07-01 12:39:46 -0400

    only show surface + add meshlib for different map layers

--- WorldFiles/GridMap.gd
+++ WorldFiles/GridMap.gd
@@ -2,17 +2,32 @@ extends GridMap
 
 var noise := FastNoiseLite.new()
 
+var height = 10
+
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# noise.seed = 3.1415
+
 	for x in range(-100,100):
-		for y in range(-10, 30):
-			for z in range(-100,100):
-				if y < noise.get_noise_2d(x, z) * 20 + 10:
-					set_cell_item(Vector3i(x,y-20,z),0)
+		for z in range(-100,100):
+			# creates randomness in [-height/2-1,height/2]
+			var y = noise.get_noise_2d(x, z) * height
+			if y < -height/2 + 2:
+				set_cell_item(Vector3i(x,y,z),0)
+				
+			elif y > -height/2 + 2 and y < height/2 - 2:
+				set_cell_item(Vector3i(x,y,z),1)
+			else:
+				set_cell_item(Vector3i(x,y,z),2)
+				
 	
-	pass # Replace with function body.
+	var grid = get_used_cells()
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	# var me = get_used_cells()
 	pass
